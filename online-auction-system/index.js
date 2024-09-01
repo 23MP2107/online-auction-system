05bf0254662ac972a5d7937b3804690a82e5701a
faathimaa <fathialick@gmail.com>
2024-09-01 18:33:13 +0530

    Update project files

--- online-auction-system/index.js
+++ online-auction-system/index.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const express = require('express')
 const app =express()
 const cookieParser = require('cookie-parser')
+const multer = require('multer'); // Add Multer for file uploads
+const path = require('path');
 
 app.set('view engine','ejs')
 app.use(cookieParser())
@@ -12,6 +14,31 @@ app.use(express.json())
 
 const connectWithDb = require('./config/database')
 connectWithDb()
+
+
+// Multer configuration for file uploads
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); // Folder where images will be stored
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname)); // Unique filename
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image/')) {
+        cb(null, true);
+    } else {
+        cb(new Error('Only images are allowed'), false);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+    fileFilter: fileFilter
+});
 
 let userRoute = require('./routes/user')
 let adminRoute = require('./routes/admin')
